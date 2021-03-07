d51ac20688adb599813f76d8647ed3d20de5d122
caixukun112 <31557344+caixukun112@users.noreply.github.com>
2021-03-07 13:56:26 +0800

    加密测试
    加密测试

--- Task/sqxs.js
+++ Task/sqxs.js
@@ -53,10 +53,40 @@ boxjs：https://raw.githubusercontent.com/xiaokxiansheng/js/master/Task/cxk10.bo
 
  */
 
-const jobname = '书旗小说'
-    const $ = Env(jobname)
 
-    let ReadTimes = 0;
+
+
+
+
+
+const jobname = '书旗小说';
+const $ = Env(jobname);
+
+
+
+/*ck解密*/
+let fs = require('fs');
+const crypto = require('crypto');
+
+function aesDecrypt(encrypted, key) {
+    const decipher = crypto.createDecipher('aes192', key);
+    let decrypted = decipher.update(encrypted, 'hex', 'utf8');
+    decrypted += decipher.final('utf8');
+    return decrypted;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+let ReadTimes = 0;
 let vediogold = 0;
 let drawgold = 0;
 
@@ -73,8 +103,11 @@ let drawgold = 0;
 async function all() {
     //nodejs运行
     if ($.isNode()) {
-
-        let sqxsck = require('./sqxsck.json');
+		let encrypted=fs.readFileSync('./sqxsck.txt', 'utf8');	
+		key = Encryptkey;
+		let decrypted = await aesDecrypt(encrypted, key);
+		sqxsck=JSON.parse(decrypted);
+        //let sqxsck = require('./sqxsck.json');
         let CountNumber = sqxsck.settings[1].val;
         $.log(`============ 共 ${CountNumber} 个${jobname}账号=============`);
         for (let i = 0; i < CountNumber; i++) {
