be49496104aec5da6d7ffe2d8de5ddaaa40638ed
cxk <31557344+caixukun112@users.noreply.github.com>
2021-09-09 09:59:08 +0800

    Update ChapterNine.js

--- Task/ChapterNine.js
+++ Task/ChapterNine.js
@@ -5,7 +5,7 @@ const $ = Env(jobname)
 
 let taskmap = new Map();
 let articleidarr = [];
-let TokenArr = ['lYj5N1IgwRVkrQ6Wneiztc9CPkSeUAo2', 'BXpKAZrCgX8vIx_XFYrwQjziaX2eRxJv'];
+let TokenArr = ['lYj5N1IgwRVkrQ6Wneiztc9CPkSeUAo2', 'BXpKAZrCgX8vIx_XFYrwQjziaX2eRxJv','g9szPKNsn7r17kq9lmWMa1YWvz3lvums'];
 !(async () => {
     await all();
 })()
