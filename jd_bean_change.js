2cdf93ada08fbb17077eda61d2b28739bc02b311
Joker0408-1 <77071343+Joker0408-1@users.noreply.github.com>
2021-09-28 05:03:15 +0800

    Update jd_bean_change.js

--- jd_bean_change.js
+++ jd_bean_change.js
@@ -5,7 +5,7 @@ Modified time: 2021-09-25 15:25:41
 cron : 0 9,19 * * * https://raw.githubusercontent.com/Joker0408-1/Script/JD/jd_bean_change.js
  */
 
-let common = require("./env/jsdom");
+let common = require("./env/jsdom.js");
 const $ = new Env('京东资产通知');
 const notify = $.isNode() ? require('./sendNotify') : '';
 const JXUserAgent =  $.isNode() ? (process.env.JX_USER_AGENT ? process.env.JX_USER_AGENT : ``):``;
