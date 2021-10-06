9bbe2b9933f0bdd942c3493eb10e5f9e54adbfd8
Joker0408-1 <77071343+Joker0408-1@users.noreply.github.com>
2021-10-07 00:01:42 +0800

    Update jd_air_validate.js

--- jd_air_validate.js
+++ jd_air_validate.js
@@ -1,6 +1,6 @@
 /*
 const $ = new Env('京东验证码获取');
-57 7,15,23 * * * https://raw.githubusercontent.com/Joker0408-1/Script/main/jd_air_validate.js
+58 7,15,23 * * * https://raw.githubusercontent.com/Joker0408-1/Script/main/jd_air_validate.js
 */
 let common = require("./function/common");
 let $ = new common.env('京东验证码获取');
