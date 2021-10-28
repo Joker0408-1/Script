c0de415956d988b241ca94a4144fa8586f8573c6
Joker0408-1 <77071343+Joker0408-1@users.noreply.github.com>
2021-10-29 03:25:53 +0800

    Update jd_joy_reward_Mod.js

--- jd_joy_reward_Mod.js
+++ jd_joy_reward_Mod.js
@@ -3,7 +3,7 @@ cron "58 7,15,23 * * *" jd_joy_reward_Mod.js
  */
 //Mod by ccwav，20211014
 // prettier-ignore
-const $ = new Env('宠汪汪积分兑换');
+const $ = new Env('宠汪汪积分兑换Mod');
 const zooFaker = require('./JDJRValidator_Aaron');
 // $.get = zooFaker.injectToRequest2($.get.bind($));
 // $.post = zooFaker.injectToRequest2($.post.bind($));
