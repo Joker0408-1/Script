8b192b8545205abc27ceff0dbbd9514e2d27968d
Joker0408-1 <77071343+Joker0408-1@users.noreply.github.com>
2021-10-29 18:36:12 +0800

    Update jd_qjd.py

--- jd_qjd.py
+++ jd_qjd.py
@@ -1,24 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*
 '''
-项目名称: JD-Script / jd_qjd
-Author: Curtin
-功能：全民抢京豆（7.22-7.31）：https://h5.m.jd.com/rn/3MQXMdRUTeat9xqBSZDSCCAE9Eqz/index.html?has_native=0
-    满160豆需要20人助力，每个用户目前只能助力2次不同的用户。
-Date: 2021/7/3 上午10:02
-TG交流 https://t.me/topstyle996
-TG频道 https://t.me/TopStyle2021
-update: 2021.7.22 16:55
-* 修复了助力活动不存在、增加了随机UA（如果未定义ua则启用随机UA）
-* 新增推送
-* 修复0点不能开团
-* 兼容pin为中文转码编码
 '''
-#print("全民抢京豆(7.2-7.15）--活动已结束\nTG交流 https://t.me/topstyle996\nTG频道 https://t.me/TopStyle2021")
-#exit(0)
-#ck 优先读取【JDCookies.txt】 文件内的ck  再到 ENV的 变量 JD_COOKIE='ck1&ck2' 最后才到脚本内 cookies=ck
 cookies = ''
-qjd_zlzh = ['Joker0408-1','Joker0409-2','Joker0410-3','Joker0411-4','建Pi_移']
+qjd_zlzh = []
 
 # Env环境设置 通知服务
 # export BARK=''                   # bark服务,苹果商店自行搜索;
