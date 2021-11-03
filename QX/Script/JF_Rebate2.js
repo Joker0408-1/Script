69fcc321df0b9ac0aeaf12271fdd976a701a26c1
Joker0408-1 <77071343+Joker0408-1@users.noreply.github.com>
2021-11-03 09:01:17 +0800

    Update JF_Rebate2.js

--- QX/Script/JF_Rebate2.js
+++ QX/Script/JF_Rebate2.js
@@ -147,7 +147,7 @@ if (url.indexOf(path2) != -1 || url.indexOf(path4) != -1) {
 function lowerMsgs(data) {
     const lower = data.lowerPriceyh;
     const lowerDate = dateFormat(data.lowerDateyh);
-        const lowerMsg = "〽️ 历史最低：¥" + String(lower) + " ║" + `🗓 ${lowerDate}`;
+        const lowerMsg = "〽️ 历史最低：¥" + String(lower) + "║" + `🗓 ${lowerDate}`;
     return lowerMsg;
 }
 
@@ -360,7 +360,7 @@ function convert(url, isOriginJXURL) {
                         let r = {};
                         let scheme = autoChoose ? autoScheme : chooseScheme;
                         if (data.data.promotionUrl) {
-                            r.msg = `💴 预计返利：¥${data.data.wlCommission}`+ " ║" + `🏷 返利率：${data.data.wlCommissionShare}%`;
+                            r.msg = `💴 预计返利：¥${data.data.wlCommission}`+ "║" + `🏷 返利率：${data.data.wlCommissionShare}%`;
                             r.convertURL =
                                 scheme == "browser"
                                     ? chooseBrowser + data.data.promotionUrl
