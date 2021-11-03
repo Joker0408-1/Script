d6aa9f053e63a56cb14fdac0be1471dcf60913f7
Joker0408-1 <77071343+Joker0408-1@users.noreply.github.com>
2021-11-03 11:16:21 +0800

    Update JF_Rebate1.js

--- QX/Script/JF_Rebate1.js
+++ QX/Script/JF_Rebate1.js
@@ -192,7 +192,7 @@ function convert(url, isOriginJXURL) {
                         let r = {};
                         let scheme = autoChoose ? autoScheme : chooseScheme;
                         if (data.data.promotionUrl) {
-                            r.msg = `💰返点比率：${data.data.wlCommissionShare} %    预计返利：¥ ${data.data.wlCommission}`;
+                            r.msg = `💴 预计返利：¥${data.data.wlCommission}`+ "║" + `🏷 返利率：${data.data.wlCommissionShare}%`;
                             r.convertURL =
                                 scheme == "browser"
                                     ? chooseBrowser + data.data.promotionUrl
