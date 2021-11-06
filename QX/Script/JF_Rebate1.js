b8d841dfca9d627245b2951bf8ad9eebc22368df
Joker0408-1 <77071343+Joker0408-1@users.noreply.github.com>
2021-11-07 01:20:53 +0800

    Update JF_Rebate1.js

--- QX/Script/JF_Rebate1.js
+++ QX/Script/JF_Rebate1.js
@@ -125,9 +125,9 @@ if (url.indexOf(path2) != -1 || url.indexOf(path4) != -1) {
     Promise.all([getHistory, convertURL, jxconvertURL])
         .then((detail) => {
             let msg = "";
-            if (detail[1] == "") detail[1] = detail[2];
+            if (detail[1] == "😤 该商品暂无返利佣金") detail[1] = detail[2];
             let convertmsg = detail[1].convertURL ? detail[1].msg : detail[1];
-            msg += convertmsg ? convertmsg : "🛍️该商品暂无佣金转链信息";
+            msg += convertmsg ? convertmsg : "😤 该商品暂无返利佣金";
             let oprnUrl = detail[1].convertURL ? detail[1].convertURL : "";
             $tool.notify("京东联盟", "", msg, oprnUrl);
         })
@@ -201,7 +201,7 @@ function convert(url, isOriginJXURL) {
                                     ? chooseBrowser + data.data.promotionUrl
                                     : `${scheme}://virtual?params=%7B%22category%22:%22jump%22,%22des%22:%22m%22,%22sourceValue%22:%22babel-act%22,%22sourceType%22:%22babel%22,%22url%22:%22${data.data.promotionUrl}%22%7D`;
                         } else {
-                            r.msg = `🛍️该商品暂无佣金转链信息，${data.data.formatContext.trim()}`;
+                            r.msg = `😤 该商品暂无返利佣金，${data.data.formatContext.trim()}`;
                             r.convertURL =
                                 scheme == "browser"
                                     ? chooseBrowser + data.data.originalContext
@@ -209,7 +209,7 @@ function convert(url, isOriginJXURL) {
                         }
                         resolve(r);
                     } else if (data.code === 105) {
-                        if (autoScheme == "openapp.jdpingou") resolve("");
+                        if (autoScheme == "openapp.jdpingou") resolve("😤 该商品暂无返利佣金");
                         else resolve("");
                     } else if (data.code === 430) {
                         $tool.setCache("false", "jfUseConvert");
