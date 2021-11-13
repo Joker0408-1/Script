c94f5ab3044565c2e8b2947ba8591c61e6948173
Joker0408-1 <77071343+Joker0408-1@users.noreply.github.com>
2021-11-14 07:26:27 +0800

    Update ql.js

--- function/ql.js
+++ function/ql.js
@@ -9,7 +9,7 @@ const qlDir = process.env.QL_DIR || '/ql';
 const authFile = path.join(qlDir, 'config/auth.json');
 
 const api = got.extend({
-  prefixUrl: process.env.QL_URL || 'http://localhost:5600',
+  prefixUrl: 'http://localhost:5600',
   retry: { limit: 0 },
 });
 
@@ -110,6 +110,16 @@ module.exports.EnableCk = async (eid) => {
     },
   }).json();
   return body;
+};
+
+module.exports.getstatus = async (eid) => {
+  const envs = await this.getEnvs();
+  for (let i = 0; i < envs.length; i++) {	 
+	if(envs[i]._id==eid){
+		 return envs[i].status; 
+	  }
+  }  
+  return 99;
 };
 
 module.exports.delEnv = async (eid) => {
