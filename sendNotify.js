934b7cb607df7990f29efac0ce87790fd914c119
Joker0408-1 <77071343+Joker0408-1@users.noreply.github.com>
2021-11-17 05:39:42 +0800

    Update sendNotify.js

--- sendNotify.js
+++ sendNotify.js
@@ -1124,9 +1124,9 @@ async function sendNotify(text, desp, params = {}, author = '') {
 
 	//提供6种通知
 	if (strAuthor)
-		desp += strAuthor ;
+		desp += '' + strAuthor;
 	else
-		desp += author ;
+		desp += author;
 
 	await serverNotify(text, desp); //微信server酱
 
@@ -1208,7 +1208,7 @@ async function sendNotifybyWxPucher(text, desp, PtPin, author = '') {
 				WP_UIDS_ONE = Uid;
 				console.log("正在发送一对一通知,请稍后...");
 				if (strAuthor)
-					desp += strAuthor;
+					desp += '' + strAuthor;
 				else
 					desp += author;
 				
