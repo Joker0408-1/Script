09f87874151af35c8169276156c1c48a269e62b5
Joker0408-1 <77071343+Joker0408-1@users.noreply.github.com>
2021-11-27 00:08:55 +0800

    Update jd_PanDaApi.py

--- jd_PanDaApi.py
+++ jd_PanDaApi.py
@@ -1,19 +1,22 @@
 """
-new Env('京东Wskey转换');
+new Env('青龙全自动更新cookie');
+cron 15 8,21 * * *
 """
-# cron:50 11,23 * * *
 
 import requests
 import time
 import json
 import re
-import uuid
 
 requests.packages.urllib3.disable_warnings()
 
 token = ""
 username = ""
 password = ""
+
+# 自定义青龙端口：
+port = ""
+
 if username == "" or password == "":
     f = open("/ql/config/auth.json")
     auth = f.read()
@@ -29,21 +32,21 @@ def gettimestamp():
 
 
 def login(username, password):
-    url = "http://127.0.0.1:5600/api/login?t=%s" % gettimestamp()
+    url = qlurl + "/api/login?t=%s" % gettimestamp()
     data = {"username": username, "password": password}
     r = s.post(url, data)
     s.headers.update({"authorization": "Bearer " + json.loads(r.text)["data"]["token"]})
 
 
 def getitem(key):
-    url = "http://127.0.0.1:5600/api/envs?searchValue=%s&t=%s" % (key, gettimestamp())
+    url = qlurl + "/api/envs?searchValue=%s&t=%s" % (key, gettimestamp())
     r = s.get(url)
     item = json.loads(r.text)["data"]
     return item
 
 
 def getckitem(key):
-    url = "http://127.0.0.1:5600/api/envs?searchValue=JD_COOKIE&t=%s" % gettimestamp()
+    url = qlurl + "/api/envs?searchValue=JD_COOKIE&t=%s" % gettimestamp()
     r = s.get(url)
     for i in json.loads(r.text)["data"]:
         if key in i["value"]:
@@ -107,7 +110,7 @@ def wstopt(wskey):
 
 
 def update(text, qlid):
-    url = "http://127.0.0.1:5600/api/envs?t=%s" % gettimestamp()
+    url = qlurl + "/api/envs?t=%s" % gettimestamp()
     s.headers.update({"Content-Type": "application/json;charset=UTF-8"})
     data = {
         "name": "JD_COOKIE",
@@ -122,7 +125,7 @@ def update(text, qlid):
 
 
 def insert(text):
-    url = "http://127.0.0.1:5600/api/envs?t=%s" % gettimestamp()
+    url = qlurl + "/api/envs?t=%s" % gettimestamp()
     s.headers.update({"Content-Type": "application/json;charset=UTF-8"})
     data = []
     data_json = {
@@ -143,6 +146,14 @@ if __name__ == '__main__':
         login(username, password)
     else:
         s.headers.update({"authorization": "Bearer " + token})
+    if port == "":
+        try:
+            r = requests.get("http://127.0.0.1:5700/login")
+            qlurl = "http://127.0.0.1:5700"
+        except:
+            qlurl = "http://127.0.0.1:5600"
+    else:
+        qlurl = "http://127.0.0.1:" + port
     wskeys = getitem("JD_WSCK")
     count = 1
     for i in wskeys:
@@ -151,31 +162,35 @@ if __name__ == '__main__':
             if r == "error":
                 print("api请求错误")
             else:
-                ptck = r
-                try:
-                    wspin = re.findall(r"pin=(.*?);", i["value"])[0]
-                    if ptck == "wskey错误":
-                        print("第%s个wskey可能过期了,pin为%s" % (count, wspin))
-                    elif ptck == "未知错误" or ptck == "error":
-                        print("第%s个wskey发生了未知错误,pin为%s" % (count, wspin))
-                    elif "</html>" in ptck:
-                        print("你的ip被cloudflare拦截")
-                    else:
-                        ptpin = re.findall(r"pt_pin=(.*?);", ptck)[0]
-                        item = getckitem("pt_pin=" + ptpin)
-                        if item != []:
-                            qlid = item["_id"]
-                            if update(ptck, qlid):
-                                print("第%s个wskey更新成功,pin为%s" % (count, wspin))
-                            else:
-                                print("第%s个wskey更新失败,pin为%s" % (count, wspin))
+                ptck = r.text
+                if r.status_code == 429:
+                    print("您的ip请求api过于频繁，已被流控")
+                    exit()
+                else:
+                    try:
+                        wspin = re.findall(r"pin=(.*?);", i["value"])[0]
+                        if ptck == "wskey错误":
+                            print("第%s个wskey可能过期了,pin为%s" % (count, wspin))
+                        elif ptck == "未知错误" or ptck == "error":
+                            print("第%s个wskey发生了未知错误,pin为%s" % (count, wspin))
+                        elif "</html>" in ptck:
+                            print("你的ip被cloudflare拦截")
                         else:
-                            if insert(ptck):
-                                print("第%s个wskey添加成功" % count)
+                            ptpin = re.findall(r"pt_pin=(.*?);", ptck)[0]
+                            item = getckitem("pt_pin=" + ptpin)
+                            if item != []:
+                                qlid = item["_id"]
+                                if update(ptck, qlid):
+                                    print("第%s个wskey更新成功,pin为%s" % (count, wspin))
+                                else:
+                                    print("第%s个wskey更新失败,pin为%s" % (count, wspin))
                             else:
-                                print("第%s个wskey添加失败" % count)
-                except:
-                    print("第%s个wskey出现异常错误" % count)
-                count += 1
+                                if insert(ptck):
+                                    print("第%s个wskey添加成功" % count)
+                                else:
+                                    print("第%s个wskey添加失败" % count)
+                    except:
+                        print("第%s个wskey出现异常错误" % count)
+                    count += 1
         else:
             print("有一个wskey被禁用了")
