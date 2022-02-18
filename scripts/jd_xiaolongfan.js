89aa3068d27e75e66cba7d51bb48938ed38f2139
star <869728223@qq.com>
2022-02-18 23:23:40 +0800

    update

--- scripts/jd_xiaolongfan.js
+++ scripts/jd_xiaolongfan.js
@@ -1,7 +1,7 @@
 /**
  一次性活动。概率蚊子腿豆子
  需要入会晓龙，差不多都入会过了。。
- cron 23 1-28 2 * * https://raw.githubusercontent.com/star261/jd/main/scripts/jd_xiaolongfan.js
+ cron 23 2 1-28 2 * https://raw.githubusercontent.com/star261/jd/main/scripts/jd_xiaolongfan.js
  */
 const $ = new Env('翻卡对对碰');
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
